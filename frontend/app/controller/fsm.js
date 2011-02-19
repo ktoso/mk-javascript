c4710f678336eaf17c918b3c0d76777c6415da61
unknown <fridek@.(none)>
2011-02-19 16:13:51 +0100

    costam

--- frontend/app/controller/fsm.js
+++ frontend/app/controller/fsm.js
@@ -5,11 +5,29 @@ app.core.Object.define("app.controller.Fsm", {
     },
     static: {},
     member: {
+		
 		character: null,
+		
+		currentState: "init",
 		
 		setCharacter: function(character) {
 			this.character = character;
-		}
+		},
 		
+		_init: function() {
+			//tutaj inicjalizacje wszystkich stanow
+		},
+		
+		requestState: function(newState) {
+			
+		},
+		
+		getState: function() {
+			return this.currentState;
+		},
+		
+		forceState: function(newState) {
+			
+		}
 	}
 });
