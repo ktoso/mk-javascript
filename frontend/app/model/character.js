db24d81fd36e8359e14ce3092335deb88f6c702a
Konrad Malawski <konrad.malawski@project13.pl>
2011-02-19 17:12:07 +0100

    charter has states and can move left/right (the switch is in place)

--- frontend/app/model/character.js
+++ frontend/app/model/character.js
@@ -1,17 +1,20 @@
 app.core.Object.define("app.model.Character", {
     extend: app.model.Object,
-    constructor: function ()
+    constructor: function (id)
     {
         arguments.callee.prototype.uper.apply(this, arguments); //call parent constructor
+        this.id = id;
     },
     static: {},
     member: {
+        id: null,
+
         health: 100,
         state: null,
 
         posX: null,
         posY: null,
 
-        directionLeft: true, //true for left, false for right
+        directionLeft: true, // true for left, false for right
     }
 });
