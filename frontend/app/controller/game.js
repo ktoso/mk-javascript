95e7f893a8e43d1b58a90f7342b3ba930dce7c53
Sebastian Poreba <sebastian@fridek.pl>
2011-02-20 11:13:49 +0100

    bind

--- frontend/app/controller/game.js
+++ frontend/app/controller/game.js
@@ -46,7 +46,9 @@ app.core.Object.define("app.controller.Game", {
 			  if(key >= 37 && key <= 40) {event.preventDefault();}
 
 			  scope.input = new app.event.Keyboard(event);
-			  scope.character.runEvent(scope.input.getCode());
+			  var code = scope.input.getCode();
+			  // false if event is not binded to any action
+			  if(code) scope.character.runEvent(code);
 			});
 		},
 	}
