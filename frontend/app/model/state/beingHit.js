dadbaef7046be0d5d61da3de2170010071333c93
Konrad <konrad.malawski@java.pl>
2011-02-20 12:37:42 +0100

    fix

--- frontend/app/model/state/beingHit.js
+++ frontend/app/model/state/beingHit.js
@@ -9,11 +9,7 @@ app.core.Object.define("app.model.state.beingHit", {
 		
 		lockTime: 0,
 		
-<<<<<<< HEAD
-		possibleTransistions: [
-=======
 		possibleTransitions: [
->>>>>>> 3441ee9c68f22bfc4565609cc3ef54ecd6760172
 			'walkRight',
 			'walkLeft',
 			'jump',
